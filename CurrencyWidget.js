d9c9263ec9a5fb9b5693742bccc86624d7b4d109
filipponaggi <71395332+filipponaggi@users.noreply.github.com>
2023-03-02 16:05:23 -0500

    Update CurrencyWidget.js

--- CurrencyWidget.js
+++ CurrencyWidget.js
@@ -31,7 +31,48 @@
     }
 
     async updateConversionRate() {
-      const from = this._props.from || "USD";
+	const http = require("https");
+	const url = 'https://api.openai.com/v1/completions';
+	const options = {
+    		method: 'POST',
+    		'Content-Type': 'application/json',
+    		'Authorization': 'Bearer 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',
+	};
+
+	const data = `{
+  		"model": "text-davinci-003",
+  		"prompt": "COME TI CHIAMI?",
+  		"temperature": 0,
+  		"max_tokens": 100,
+		"top_p": 1,
+  		"frequency_penalty": 0.0,
+  		"presence_penalty": 0.0,
+  		"stop": ["n"]
+	}`;
+
+	let result = '';
+	const req = http.request(url, options, (res) => {
+    		console.log(res.statusCode);
+		res.setEncoding('utf8');
+    		res.on('data', (chunk) => {
+        	result += chunk;
+    	});
+
+    	res.on('end', () => {
+        console.log(result);
+    	});
+	});
+
+	req.on('error', (e) => {
+    	console.error(e);
+	});
+
+	req.write(data);
+	req.end();
+	    
+	    
+	    
+      /* const from = this._props.from || "USD";
       const to = this._props.to || "INR";
       const amount = this._props.amount || 1 ;
       const decimalPlaces = this._props.decimalPlaces || 2;
@@ -42,7 +83,7 @@
 
       const conversionRate = data.result.toFixed(decimalPlaces);
       const conversionRateElement = this.shadowRoot.getElementById("conversionRate");
-      conversionRateElement.innerText = `${conversionRate} ${to}/${from}`;
+      conversionRateElement.innerText = `${conversionRate} ${to}/${from}`;*/
     }
 
     onCustomWidgetBeforeUpdate(changedProperties) {
